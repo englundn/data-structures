b16abdcad342023f79e6e55bc587785484fc345d
englundn <englundn@gmail.com>
2016-09-15 11:01:25 -0700

    Finished hashTable functionality.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,22 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.getIndexBelowMaxForKey = getIndexBelowMaxForKey;
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
