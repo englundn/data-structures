9aa1844a2be428d802fe6bbbc4a4b7b26d3dbc77
englundn <englundn@gmail.com>
2016-09-14 19:59:52 -0700

    Finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+    if (list.head) {
+      var currentNode = list.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = Node(value);
+    } else {
+      list.head = newTail;
+    }
+    list.tail = Node(value);
+    // debugger;
   };
 
   list.removeHead = function() {
+    var headVal = list.head.value;
+    list.head = list.head.next;
+    console.log(list);
+    return headVal;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode.next) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return currentNode.value === target;
   };
 
   return list;
