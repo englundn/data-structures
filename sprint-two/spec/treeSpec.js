87bffb1832c1d619473c56e8022f652d6f07d202
englundn <englundn@gmail.com>
2016-09-15 20:15:47 -0700

    Updated tests for Tree to test property "parent" and method "removeFromParent".

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -5,10 +5,12 @@ describe('tree', function() {
     tree = Tree();
   });
 
-  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+  it('should have methods named "addChild", "removeFromParent", and "contains", and properties named "value" and "parent"', function() {
     expect(tree.addChild).to.be.a('function');
     expect(tree.contains).to.be.a('function');
+    expect(tree.removeFromParent).to.be.a('function');
     expect(tree.hasOwnProperty('value')).to.equal(true);
+    expect(tree.hasOwnProperty('parent')).to.equal(true);
   });
 
   it('should add children to the tree', function() {
@@ -40,5 +42,15 @@ describe('tree', function() {
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
   });
+
+  it('should correctly remove from parent', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+    tree.children[0].removeFromParent();
+    expect(tree.parent).to.equal(null);    
+  });
+
 
 });
